973da8d1ca6f308a87c605e5c3f4f07f0294151b
anggela-lib <anggela.acc@gmail.com>
2025-12-07 20:42:02 +0700

    Android ver

---
+++ android/app/src/main/java/com/evoria/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.evoria.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.evoria.app',
+  appName: 'evoria',
+  webDir: 'build'
+};
+
+export default config;
